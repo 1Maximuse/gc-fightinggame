8bfd65e16858c94ccc542eaeab58dc4a96d93d73
Emmanuel Maximus Yohanes <geofanny.emmanuel@gmail.com>
2021-04-07 16:06:22 +0700

    update collision

--- Assets/PlayerScript.cs
+++ Assets/PlayerScript.cs
@@ -4,6 +4,15 @@ using UnityEngine;
 
 public class PlayerScript : MonoBehaviour
 {
+    [SerializeField]
+    private bool controlled;
+
+    [SerializeField]
+    private float minDistance;
+
+    [SerializeField]
+    private Transform otherPlayer;
+
     private Animator animator;
     private Quaternion initialRotation;
 
@@ -27,32 +36,40 @@ public class PlayerScript : MonoBehaviour
     {
         transform.position = new Vector3(0f, 0f, transform.position.z);
         transform.rotation = initialRotation;
-        if (Input.GetKeyDown(KeyCode.D))
+        if (controlled)
         {
-            animator.SetBool("Forward", true);
-        }
-        if (Input.GetKeyUp(KeyCode.D))
-        {
-            animator.SetBool("Forward", false);
-        }
+            if (Input.GetKeyDown(KeyCode.D))
+            {
+                animator.SetBool("Forward", true);
+            }
+            if (Input.GetKeyUp(KeyCode.D))
+            {
+                animator.SetBool("Forward", false);
+            }
 
-        if (Input.GetKeyDown(KeyCode.A))
-        {
-            animator.SetBool("Backward", true);
-        }
-        if (Input.GetKeyUp(KeyCode.A))
-        {
-            animator.SetBool("Backward", false);
-        }
+            if (Input.GetKeyDown(KeyCode.A))
+            {
+                animator.SetBool("Backward", true);
+            }
+            if (Input.GetKeyUp(KeyCode.A))
+            {
+                animator.SetBool("Backward", false);
+            }
 
-        if (Input.GetMouseButtonDown(0))
-        {
-            animator.SetTrigger("Jab");
-        }
+            if (Input.GetMouseButtonDown(0))
+            {
+                animator.SetTrigger("Jab");
+            }
 
-        if (Input.GetMouseButtonDown(1))
+            if (Input.GetMouseButtonDown(1))
+            {
+                animator.SetTrigger("Kick");
+            }
+        }
+        if (Mathf.Abs(transform.position.z - otherPlayer.position.z) < minDistance)
         {
-            animator.SetTrigger("Kick");
+            bool direction = (transform.position.z - otherPlayer.position.z) > 0;
+            transform.position = transform.position - new Vector3(0f, 0f, 10f * Time.deltaTime * (direction ? -1 : 1));
         }
     }
 }
